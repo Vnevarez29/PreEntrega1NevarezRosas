1995aedb0972c0a10873e76616d05725ef5304b3
Vianey Nevárez <vnevarezrosas@gmail.com>
2024-03-24 23:29:20 -0500

    se agrega conexion a firebase

--- proyectoNevarezRosas/src/main.jsx
+++ proyectoNevarezRosas/src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZFPiihzV47pcl1_IO4wGcxFCFXpLrh_g",
+  authDomain: "pruebacoderreact.firebaseapp.com",
+  projectId: "pruebacoderreact",
+  storageBucket: "pruebacoderreact.appspot.com",
+  messagingSenderId: "784337455528",
+  appId: "1:784337455528:web:ce9cffbbb53e5795c2359d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db= getFireStore(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
